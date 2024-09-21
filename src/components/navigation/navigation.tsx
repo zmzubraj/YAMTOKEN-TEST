d0f8bc50db6f5db8e94574bf9b82bb73cf80d2dc
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-09-22 00:32:01 +0600

    Refactor navigation component and connect-wallet utility

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,115 +1,111 @@
-import React, { FC, useState } from "react";
-import Box from "@mui/material/Box";
-import { navigations } from "./navigation.data";
-import { Link } from "@mui/material";
-import { useLocation } from "react-router-dom";
-import metamask from '@/utils/connect-wallet'
+import React, { FC, useState } from 'react';
+import Box from '@mui/material/Box';
+import { navigations } from './navigation.data';
+import { Link } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import metamask from '../../utils/connect-wallet';
 
 type NavigationData = {
-  path: string;
-  label: string;
+	path: string;
+	label: string;
 };
 
 const Navigation: FC = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
-  const [connected, setConnected] = useState(false)
-  const [address, setAddress] = useState('')
+	const location = useLocation();
+	const currentPath = location.pathname;
+	const [connected, setConnected] = useState(false);
+	const [address, setAddress] = useState('');
 
-  const connectWallet = async () => {
-    if (!connected){
-      console.log('connect')
-      const address = await metamask.connect()
-      setConnected(true)
-      setAddress(address)
-      return
-    }
-    else{
-      console.log('disconnect')
-      await metamask.disconnect()
-      setConnected(false)
-      setAddress('')
-      return
-    }
-  }
+	const connectWallet = async () => {
+		if (!connected) {
+			console.log('connect');
+			const address = await metamask.connect();
+			setConnected(true);
+			setAddress(address);
+			return;
+		} else {
+			console.log('disconnect');
+			await metamask.disconnect();
+			setConnected(false);
+			setAddress('');
+			return;
+		}
+	};
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexFlow: "wrap",
-        justifyContent: "end",
-        flexDirection: { xs: "column", lg: "row" }
-      }}
-    >
-      {navigations.map(({ path: destination, label }: NavigationData) =>
-        <Box
-          key={label}
-          component={Link}
-          href={destination}
-          sx={{
-            display: "inline-flex",
-            position: "relative",
-            color: currentPath === destination ? "" : "white",
-            lineHeight: "30px",
-            letterSpacing: "3px",
-            cursor: "pointer",
-            textDecoration: "none",
-            textTransform: "uppercase",
-            fontWeight: 700,
-            alignItems: "center",
-            justifyContent: "center",
-            px: { xs: 0, lg: 3 },
-            mb: { xs: 3, lg: 0 },
-            fontSize: "20px",
-            ...destination === "/" && { color: "primary.main" },
-            "& > div": { display: "none" },
-            "&.current>div": { display: "block" },
-            "&:hover": {
-              color: "text.disabled"
-            }
-          }}
-        >
-          <Box
-            sx={{
-              position: "absolute",
-              top: 12,
-              transform: "rotate(3deg)",
-              "& img": { width: 44, height: "auto" }
-            }}
-          >
-            {/* eslint-disable-next-line */}
-            <img src="/images/headline-curve.svg" alt="Headline curve" />
-          </Box>
-          {label}
-        </Box>
-      )}
-      <Box
-        sx={{
-          position: "relative",
-          color: "white",
-          cursor: "pointer",
-          textDecoration: "none",
-          textTransform: "uppercase",
-          fontWeight: 600,
-          display: "inline-flex",
-          alignItems: "center",
-          justifyContent: "center",
-          px: { xs: 0, lg: 3 },
-          mb: { xs: 3, lg: 0 },
-          fontSize: "24px",
-          lineHeight: "6px",
-          width: "324px",
-          height: "45px",
-          borderRadius: "6px",
-          backgroundColor: "#00dbe3"
-        }}
-        onClick={connectWallet}
-      >
-        {connected ? "Disconnect Wallet" : "Connect Wallet"}
-      </Box>
-    </Box>
-  );
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexFlow: 'wrap',
+				justifyContent: 'end',
+				flexDirection: { xs: 'column', lg: 'row' },
+			}}>
+			{navigations.map(({ path: destination, label }: NavigationData) => (
+				<Box
+					key={label}
+					component={Link}
+					href={destination}
+					sx={{
+						display: 'inline-flex',
+						position: 'relative',
+						color: currentPath === destination ? '' : 'white',
+						lineHeight: '30px',
+						letterSpacing: '3px',
+						cursor: 'pointer',
+						textDecoration: 'none',
+						textTransform: 'uppercase',
+						fontWeight: 700,
+						alignItems: 'center',
+						justifyContent: 'center',
+						px: { xs: 0, lg: 3 },
+						mb: { xs: 3, lg: 0 },
+						fontSize: '20px',
+						...(destination === '/' && { color: 'primary.main' }),
+						'& > div': { display: 'none' },
+						'&.current>div': { display: 'block' },
+						'&:hover': {
+							color: 'text.disabled',
+						},
+					}}>
+					<Box
+						sx={{
+							position: 'absolute',
+							top: 12,
+							transform: 'rotate(3deg)',
+							'& img': { width: 44, height: 'auto' },
+						}}>
+						{/* eslint-disable-next-line */}
+						<img src="/images/headline-curve.svg" alt="Headline curve" />
+					</Box>
+					{label}
+				</Box>
+			))}
+			<Box
+				sx={{
+					position: 'relative',
+					color: 'white',
+					cursor: 'pointer',
+					textDecoration: 'none',
+					textTransform: 'uppercase',
+					fontWeight: 600,
+					display: 'inline-flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					px: { xs: 0, lg: 3 },
+					mb: { xs: 3, lg: 0 },
+					fontSize: '24px',
+					lineHeight: '6px',
+					width: '324px',
+					height: '45px',
+					borderRadius: '6px',
+					backgroundColor: '#00dbe3',
+				}}
+				onClick={connectWallet}>
+				{connected ? 'Disconnect Wallet' : 'Connect Wallet'}
+			</Box>
+			{connected && <div>{address}</div>}
+		</Box>
+	);
 };
 
 export default Navigation;
--- src/utils/connect-wallet.ts
+++ src/utils/connect-wallet.ts
@@ -1,27 +1,27 @@
 declare global {
-    interface Window {
-      ethereum?: any
-      unisat?: any
-    }
-  }
-  
-class Metamask {
-  constructor() {}
-
-  public async connect() {
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    })
-
-    return accounts[0]
-  }
-
-  public async disconnect() {
-    await window.ethereum.request({
-      method: 'wallet_revokePermissions',
-      params: [{ eth_accounts: {} }],
-    })
-  }
+	interface Window {
+		ethereum?: any;
+		unisat?: any;
+	}
 }
 
-export default new Metamask()
+class Metamask {
+	public async connect() {
+		const accounts = await window.ethereum.request({
+			method: 'eth_requestAccounts',
+		});
+
+		return accounts[0];
+	}
+
+	public async disconnect() {
+		await window.ethereum.request({
+			method: 'wallet_revokePermissions',
+			params: [{ eth_accounts: {} }],
+		});
+	}
+}
+
+const metamask = new Metamask();
+
+export default metamask;
